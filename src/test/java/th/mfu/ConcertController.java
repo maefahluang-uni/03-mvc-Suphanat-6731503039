65f5f4081ae1bacfff7195fcf5eb628710b91465
Suphanat-6731503039 <6731503039@lamduan.mfu.ac.th>
2025-08-25 09:48:30 +0000

    done

--- src/test/java/th/mfu/ConcertController.java
+++ src/test/java/th/mfu/ConcertController.java
@@ -1,11 +1,11 @@
 package th.mfu;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
@@ -19,58 +19,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ConcertController {
-    // TODO: create hashmap of concerts for storing data
-    private HashMap<Integer, Concert> concertMap = new HashMap<>();
+    private HashMap<Integer, Concert> concerts = new HashMap<>();
     private int nextId = 1;
-    //TODO: add initbinder to convert date
+
     @InitBinder
     public void initBinder(WebDataBinder binder) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-        dateFormat.setLenient(false);
-        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        sdf.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, true));
     }
+
     @GetMapping("/concerts")
     public String listConcerts(Model model) {
-        // TODO: add concerts to model
-        model.addAttribute("concerts", new ArrayList<Concert>(concertMap.values()));
-        // TODO: return a template to list concerts
-        return "list-concert";
+        List<Concert> concertList = new ArrayList<>(concerts.values());
+        model.addAttribute("concerts", concertList);
+        return "list-concert"; // match test expectation
     }
 
     @GetMapping("/add-concert")
     public String addAConcertForm(Model model) {
-        // TODO: pass blank concert to a form
         model.addAttribute("concert", new Concert());
-        // TODO: return a template for concert form
-        return "add-concert-form";
+        return "concert-form";
     }
 
     @PostMapping("/concerts")
     public String saveConcert(@ModelAttribute Concert concert) {
-        // TODO: add concert to list of concerts
-        concert.setId(nextId++);
-        // TODO: increment nextId
-        concertMap.put(concert.getId(), concert);
-        // TODO: redirect to list concerts
+        concerts.put(nextId++, concert);
         return "redirect:/concerts";
     }
 
     @GetMapping("/delete-concert/{id}")
     public String deleteConcert(@PathVariable int id) {
-        // TODO: remove concert from list of concerts
-        concertMap.remove(id);
-        // TODO: redirect to list concerts
+        concerts.remove(id);
         return "redirect:/concerts";
     }
 
-    
     @GetMapping("/delete-concert")
     public String removeAllConcerts() {
-        //TODO: clear all employees and reset id
-        concertMap.clear();
+        concerts.clear();
         nextId = 1;
-        // TODO: redirect to list concerts
         return "redirect:/concerts";
     }
-
 }
